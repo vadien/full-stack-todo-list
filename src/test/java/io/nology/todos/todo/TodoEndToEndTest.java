448a8c2dc7cb9d3f663a6ab366139baa1ff1db1f
vadien <david.c.neill@gmail.com>
2024-09-04 12:08:18 +1000

    Adds github workflow

--- src/test/java/io/nology/todos/todo/TodoEndToEndTest.java
+++ src/test/java/io/nology/todos/todo/TodoEndToEndTest.java
@@ -6,9 +6,7 @@ import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInC
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
